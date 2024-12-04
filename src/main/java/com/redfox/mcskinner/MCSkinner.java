eb8007613c33d000a25d72a84cd269569a3c5cf1
DieRednax <xt.delange@gmail.com>
2024-12-04 21:14:00 +0200

    SkinPackGen: single skin in skins.json

--- src/main/java/com/redfox/mcskinner/MCSkinner.java
+++ src/main/java/com/redfox/mcskinner/MCSkinner.java
@@ -1,7 +1,19 @@
 package com.redfox.mcskinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MCSkinner {
     public static void main(String[] args) {
+        HashMap<String, String> testSkin = new HashMap<>();
+            testSkin.put("name", "test Skin");
+            testSkin.put("geo", "classic");
+            testSkin.put("texture", "C:\\Users\\<user>\\OneDrive\\Minecraft\\Resource packs\\Custom Capes By BionicBen.zip\\Custom Capes By BionicBen\\bionic.png"); //replace <user>
+            testSkin.put("cape", "C:\\Users\\<user>\\OneDrive\\Minecraft\\Resource packs\\Custom Capes By BionicBen.zip\\Custom Capes By BionicBen\\Bee.png"); //replace <user>
+        ArrayList<HashMap<String, String>> test = new ArrayList<>();
+        test.add(testSkin);
 
+        String json = new SkinPackGen(test,"test").genSkinsJSON();
+        System.out.println(json);
     }
 }
--- src/main/java/com/redfox/mcskinner/SkinPackGen.java
+++ src/main/java/com/redfox/mcskinner/SkinPackGen.java
@@ -1,13 +1,84 @@
 package com.redfox.mcskinner;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SkinPackGen {
-    private ArrayList<HashMap<String, Object>> skins;
+    private ArrayList<HashMap<String, String>> skins;
     private String name;
-    public SkinPackGen(ArrayList<HashMap<String, Object>> skins, String name) {
+    public SkinPackGen(ArrayList<HashMap<String, String>> skins, String name) {
         this.skins = skins;
         this.name = name;
+    }
+    public String genSkinsJSON() {
+        StringBuilder skinsJSON = new StringBuilder();
+        skinsJSON.append("[\n");
+
+        for (HashMap<String, String> skin : skins) {
+            skinsJSON.append("\t{\n");
+
+            int cursor = 0;
+            for (String key : skin.keySet()) {
+                cursor++;
+                switch (key) {
+                    case "name":
+                        skinsJSON.append(
+                                appendStatement("localization_name",
+                                        skin.get(key).toLowerCase().replaceAll("\\s", "_"))
+                        ).append(",\n");
+                        break;
+                    case "geo":
+                        String geo;
+
+                        if (skin.get(key).equals("classic")) {
+                            geo = "geometry.humanoid.custom";
+                        } else if (skin.get(key).equals("slim")) {
+                            geo = "geometry.humanoid.customSlim";
+                        } else geo = skin.get(key);
+
+                        skinsJSON.append(
+                                appendStatement("geometry", geo)
+                        ).append(",\n");
+                        break;
+                    case "texture":
+                        skinsJSON.append(
+                                appendStatement("texture",
+                                        skin.get(key).replace(new File(skin.get(key)).getParent(), "")
+                                                .replace("/", "")
+                                                .replace("\\", ""))
+                        ).append(",\n");
+                        break;
+                    case "cape":
+                        skinsJSON.append(
+                                appendStatement("cape",
+                                        skin.get(key).replace(new File(skin.get(key)).getParent(), "")
+                                                .replace("/", "")
+                                                .replace("\\", ""))
+                        ).append(",\n");
+                        break;
+                }
+
+//                if (cursor < skin.keySet().size()) {
+//                    skinsJSON.append(",\n");
+//                } else if (cursor == skin.keySet().size()) {
+//                    skinsJSON.append("\n");
+//                }
+            }
+            skinsJSON.append(
+                    appendStatement("type", "free")
+            ).append("\n");
+        }
+
+        skinsJSON.append("\t},\n");
+
+        skinsJSON.append("]");
+
+        return skinsJSON.toString();
+    }
+
+    //json gen
+    private String appendStatement(String left, String right) {
+        return "\t\t\"" + left + "\": \"" + right + "\"";
     }
 }
